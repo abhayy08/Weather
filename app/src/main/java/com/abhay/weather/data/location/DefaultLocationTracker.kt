985da4ca7863472708e6eae14ab2c982dc01f0c9
Abhay <abhayy.sharma.08@gmail.com>
2023-04-27 18:02:31 +0530

    Fixing location implementation

--- app/src/main/java/com/abhay/weather/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/com/abhay/weather/data/location/DefaultLocationTracker.kt
@@ -6,16 +6,20 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.abhay.weather.domain.location.LocationTracker
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationListener
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
@@ -39,47 +43,53 @@ class DefaultLocationTracker @Inject constructor(
 
         val locationManager =
             application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
         if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
             return null
         }
 
-        val request = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
-            .setMinUpdateIntervalMillis(2000)
-            .setMaxUpdateDelayMillis(5000)
-            .setWaitForAccurateLocation(false)
+        val request = LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY, 1000)
             .build()
 
-        val locationCallBack = object : LocationCallback() {
-            override fun onLocationResult(result: LocationResult) {
-                super.onLocationResult(result)
-            }
-        }
+        val deferred = CompletableDeferred<Location?>()
 
-        locationClient
-            .requestLocationUpdates(request, locationCallBack, null)
-
-        return suspendCancellableCoroutine { cont ->
-            locationClient.lastLocation.apply {
-                if (isComplete) {
-                    if (isSuccessful) {
-                        cont.resume(result)
-                    } else {
-                        cont.resume(null)
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult) {
+                    result ?: return
+                    val location = result.locations.firstOrNull()
+                    location?.let {
+                        deferred.complete(it)
+                        Log.d("Location123", "Latitude: ${it.latitude}, Longitude: ${it.longitude}")
+                        locationClient.removeLocationUpdates(this)
                     }
-                    return@suspendCancellableCoroutine
-                }
-                addOnSuccessListener {
-                    cont.resume(it)
-                }
-                addOnFailureListener {
-                    cont.resume(null)
-                }
-                addOnCanceledListener {
-                    cont.cancel()
                 }
             }
-        }
+
+           locationClient.requestLocationUpdates(request, locationCallback, null)
+
+            return deferred.await()
+//        suspendCancellableCoroutine { cont ->
+//            locationClient.lastLocation.apply {
+//                if (isComplete) {
+//                    if (isSuccessful) {
+//                        cont.resume(result)
+//                    } else {
+//                        cont.resume(null)
+//                    }
+//                    return@suspendCancellableCoroutine
+//                }
+//                addOnSuccessListener {
+//                    cont.resume(it)
+//                }
+//                addOnFailureListener {
+//                    cont.resume(null)
+//                }
+//                addOnCanceledListener {
+//                    cont.cancel()
+//                }
+//            }
+//        }
     }
 }
